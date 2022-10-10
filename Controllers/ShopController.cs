aca0d63a0390617f6da030bf31825f2c1a43b109
brunoHao <106672020+brunoHao@users.noreply.github.com>
2022-10-10 15:40:48 -0700

    FinishShowCategory

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,19 +1,31 @@
 ﻿using DemoWebTemplate.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoWebTemplate.Controllers
 {
     public class ShopController : Controller
     {
         private readonly MyDatabase _myDatabase;
+        private readonly ILogger<ShopController> _logger;
 
-        public ShopController(MyDatabase myDatabase)
+        public ShopController(MyDatabase myDatabase,ILogger<ShopController> logger)
         {
             _myDatabase = myDatabase;
+            _logger = logger;
         }
-        public IActionResult Category()
+
+        public IActionResult Category(int Id)
         {
-            return View(_myDatabase.Products.ToList());
+            ViewBag.categoryType = _myDatabase.Categories.ToList();
+            if (Id == 0)
+            {
+                return View(_myDatabase.Products.ToList());
+            }
+            else
+            {
+                return View(_myDatabase.Products.Where(x => x.Category.Id == Id).ToList());
+            }
         }
         public IActionResult Singleproduct()
         {
