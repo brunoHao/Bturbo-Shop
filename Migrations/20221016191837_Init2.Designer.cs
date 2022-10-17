5751248e00844d5af0369324c7c877a12c2ff851
brunoHao <106672020+brunoHao@users.noreply.github.com>
2022-10-17 12:28:42 -0700

    FixCartAlready
    - Finish column in View Cart

--- Migrations/20221016191837_Init2.Designer.cs
+++ Migrations/20221016191837_Init2.Designer.cs
@@ -4,6 +4,7 @@ using DemoWebTemplate.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoWebTemplate.Migrations
 {
     [DbContext(typeof(MyDatabase))]
-    partial class MyDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20221016191837_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
